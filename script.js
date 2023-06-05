a9c359149d05e1e823cd0105da04d052294984f1
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-06-05 20:00:04 +0200

    Coding Challenge 1 finished

--- script.js
+++ script.js
@@ -249,43 +249,43 @@ GOOD LUCK 😀
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section!
+  // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
-
-  // Get answer
   registerNewAnswer() {
+    // Get answer
     const answer = Number(
       prompt(
-        `${this.question}\n${this.options.join(`\n`)}\n(Write option number)`
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
       )
     );
     console.log(answer);
 
-    //Register answer
-typeof answer === `number` && answer < this.answers.length && this.answers[answer]++;
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
 
-this.displayResults();
-this.displayResults(`string`);
+    this.displayResults();
+    this.displayResults('string');
+  },
 
   displayResults(type = 'array') {
     if (type === 'array') {
       console.log(this.answers);
-    }else if (type === `string`){
+    } else if (type === 'string') {
       // Poll results are 13, 2, 4, 1
-      console.log(`Poll results are ${this.answers.join(`, `)}`);
-  }
-}
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
 };
 
-// document.querySelector('.poll').addEventListener('click', function () {
-//   poll.registerNewAnswer();
-// });
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
-
-
-// Display test data arrays using the displayResults method
-poll.displayResults.call({ answers: [5, 2, 3] }, 'array');
 poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'array');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
